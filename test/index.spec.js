a772da36f8f4f70665425c4c223cb25b758ebb2f
Resi Respati <resir014@gmail.com>
2017-02-27 11:37:58 +0700

    Reworked test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,43 +8,43 @@ var testPassword = 'password'
 var testSite = 'example.com'
 
 describe('mpw', function () {
-  describe('v3', function () {
-    it('generates the correct `maximum` template password', function () {
+  describe('#generatePassword()', function () {
+    it('generates the correct `maximum` template password (V3)', function () {
       var testKey = mpw.generateKey(testUsername, testPassword)
       var testSeed = mpw.generateSeed(testSite, testKey)
 
       var gen = mpw.generatePassword(testSite, testKey, testSeed, 1, 'maximum')
       assert.equal(gen, 'v8]RfHIhBg1XE1!692*O')
     })
-    it('generates the correct `long` template password', function () {
+    it('generates the correct `long` template password (V3)', function () {
       var testKey = mpw.generateKey(testUsername, testPassword)
       var testSeed = mpw.generateSeed(testSite, testKey)
 
       var gen = mpw.generatePassword(testSite, testKey, testSeed, 1, 'long')
       assert.equal(gen, 'Fogu4!ZindLoni')
     })
-    it('generates the correct `medium` template password', function () {
+    it('generates the correct `medium` template password (V3)', function () {
       var testKey = mpw.generateKey(testUsername, testPassword)
       var testSeed = mpw.generateSeed(testSite, testKey)
 
       var gen = mpw.generatePassword(testSite, testKey, testSeed, 1, 'medium')
       assert.equal(gen, 'Fog9-Qeb')
     })
-    it('generates the correct `short` template password', function () {
+    it('generates the correct `short` template password (V3)', function () {
       var testKey = mpw.generateKey(testUsername, testPassword)
       var testSeed = mpw.generateSeed(testSite, testKey)
 
       var gen = mpw.generatePassword(testSite, testKey, testSeed, 1, 'short')
       assert.equal(gen, 'Fog9')
     })
-    it('generates the correct `basic` template password', function () {
+    it('generates the correct `basic` template password (V3)', function () {
       var testKey = mpw.generateKey(testUsername, testPassword)
       var testSeed = mpw.generateSeed(testSite, testKey)
 
       var gen = mpw.generatePassword(testSite, testKey, testSeed, 1, 'basic')
       assert.equal(gen, 'vVv9fOp2')
     })
-    it('generates the correct `pin` template password', function () {
+    it('generates the correct `pin` template password (V3)', function () {
       var testKey = mpw.generateKey(testUsername, testPassword)
       var testSeed = mpw.generateSeed(testSite, testKey)
 
