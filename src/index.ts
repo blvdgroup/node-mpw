ecbac4ce8663723edda3287f96cb5a996488556e
Resi Respati <resir014@gmail.com>
2017-03-01 13:01:37 +0700

    :bulb: V2 backwards compatibility

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,30 @@ import * as constants from './utils/constants'
 import { generateSeed } from './utils/helpers'
 import { templates, templateChars } from './utils/templates'
 
-export const generateKey = (name: string, password: string, namespace?: string, version?: string): Buffer => {
+export const generateKey = (name: string, password: string, namespace?: string, version?: number): Buffer => {
   if (!namespace) {
     namespace = constants.NAMESPACE
   }
+  if (!version) {
+    version = constants.MP_ALGORITHM_VERSION
+  }
+
+  // Cache name length for older versions of MPW.
+  // https://github.com/tmthrgd/mpw-js/blob/master/mpw.js#L36-L38
+  let nameLength = name.length
 
   // Salt for the scrypt function: `namespace + name.length + name`
-  const buf = Buffer.allocUnsafe(namespace.length + 4 + name.length)
-  buf.fill(0)
+  const buf = Buffer.allocUnsafe(namespace.length + 4 + name.length).fill(0)
 
   buf.write(namespace)
-  buf.writeUInt32BE(name.length, namespace.length)
+
+  // V2 backwards compatibility: use the cached nameLength for older versions.
+  if (version < 3) {
+    buf.writeUInt32BE(nameLength, namespace.length)
+  } else {
+    buf.writeUInt32BE(name.length, namespace.length)
+  }
+
   buf.write(name, namespace.length + 4)
 
   return scrypt.hashSync(password, constants.SCRYPT_PARAMS, 64, buf)
--- test/index.spec.js
+++ test/index.spec.js
@@ -22,13 +22,31 @@ describe('mpw', function () {
     it('generates the correct `medium` template password (V3)', function () {
       assert.equal(mpw.generatePassword(testSite, testKey, 1, 'medium'), 'Fog9-Qeb')
     })
-    it('generates the correct `short` template password (V3)', function () {
-      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'short'), 'Fog9')
-    })
     it('generates the correct `basic` template password (V3)', function () {
       assert.equal(mpw.generatePassword(testSite, testKey, 1, 'basic'), 'vVv9fOp2')
     })
+    it('generates the correct `short` template password (V3)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'short'), 'Fog9')
+    })
     it('generates the correct `pin` template password (V3)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'pin'), '5819')
+    })
+    it('generates the correct `maximum` template password (V2)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'maximum'), 'v8]RfHIhBg1XE1!692*O')
+    })
+    it('generates the correct `long` template password (V2)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'long'), 'Fogu4!ZindLoni')
+    })
+    it('generates the correct `medium` template password (V2)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'medium'), 'Fog9-Qeb')
+    })
+    it('generates the correct `basic` template password (V2)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'basic'), 'vVv9fOp2')
+    })
+    it('generates the correct `short` template password (V2)', function () {
+      assert.equal(mpw.generatePassword(testSite, testKey, 1, 'short'), 'Fog9')
+    })
+    it('generates the correct `pin` template password (V2)', function () {
       assert.equal(mpw.generatePassword(testSite, testKey, 1, 'pin'), '5819')
     })
   })
